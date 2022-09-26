788192de02adcc225dd0475a28b88c939c808ca7
ZingerLittleBee <6970999@gmail.com>
2022-09-26 23:53:25 +0800

    feat: 🎸 disk net sort

--- src/vo/realtime_status.rs
+++ src/vo/realtime_status.rs
@@ -1,10 +1,10 @@
+use crate::model::realtime_status::RealtimeStatus;
 use crate::vo::cpu::CpuUsageVo;
 use crate::vo::disk::DiskDetailVo;
-use crate::vo::network::NetworkDetailVo;
-use serde::{Deserialize, Serialize};
-use crate::model::realtime_status::RealtimeStatus;
 use crate::vo::formator::Convert;
+use crate::vo::network::NetworkDetailVo;
 use crate::vo::process::ProcessVo;
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize, Default, Debug)]
 pub struct RealtimeStatusVo {
@@ -18,12 +18,30 @@ pub struct RealtimeStatusVo {
 
 impl Convert<RealtimeStatusVo> for RealtimeStatus {
     fn convert(&self) -> RealtimeStatusVo {
+        let mut network = self
+            .network
+            .iter()
+            .map(|network| network.convert())
+            .collect::<Vec<NetworkDetailVo>>();
+        network.sort_by_key(|net| net.name.to_lowercase());
+
+        let mut disk = self
+            .disk
+            .iter()
+            .map(|disk| disk.convert())
+            .collect::<Vec<DiskDetailVo>>();
+        disk.sort_by_key(|disk| disk.device_name.to_lowercase());
+
         RealtimeStatusVo {
             cpu: self.cpu.iter().map(|cpu| cpu.convert()).collect(),
             load_avg: self.load_avg.clone(),
-            process: self.process.iter().map(|process| process.convert()).collect(),
-            network: self.network.iter().map(|network| network.convert()).collect(),
-            disk: self.disk.iter().map(|disk| disk.convert()).collect(),
+            process: self
+                .process
+                .iter()
+                .map(|process| process.convert())
+                .collect(),
+            network,
+            disk,
             uptime: self.uptime.clone(),
         }
     }
