713bd50ab0b583fd10580984c886f793d4d78fc1
ZingerLittleBee <6970999@gmail.com>
2023-08-22 03:21:02 +0800

    feat: 🎸 auth support url param

--- web/src/token/communication_token.rs
+++ web/src/token/communication_token.rs
@@ -1,8 +1,9 @@
-use actix_web::{FromRequest, HttpRequest, dev::Payload};
 use actix_web::web::Data;
+use actix_web::{dev::Payload, FromRequest, HttpRequest};
 use log::warn;
 use serde::{Deserialize, Serialize};
 use sled::Db;
+use std::collections::HashMap;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct CommunicationToken(String);
@@ -16,7 +17,8 @@ impl CommunicationToken {
             }
             false
         } else {
-            db.insert(CommunicationToken::token_key(), self.0.as_bytes()).unwrap();
+            db.insert(CommunicationToken::token_key(), self.0.as_bytes())
+                .unwrap();
             true
         };
     }
@@ -33,26 +35,52 @@ impl FromRequest for CommunicationToken {
     fn from_request(req: &HttpRequest, _payload: &mut Payload) -> Self::Future {
         let db = req.app_data::<Data<Db>>().unwrap();
 
-        let header_value = req.headers().get("Authorization");
-        if let Some(header_value) = header_value {
-            let token_str = header_value.to_str().unwrap().to_owned();
-            let token = CommunicationToken(token_str);
-            if token.is_valid(db) {
-                futures_util::future::ready(Ok(token))
-            } else {
-                warn!("Token: {} is invalid, request from: {}",
-                    header_value.to_str().unwrap_or(""), req.connection_info().realip_remote_addr().unwrap_or("unknown"));
-                futures_util::future::ready(Err(actix_web::error::ErrorUnauthorized("Token is invalid")))
-            }
-        } else {
+        let query_string = req.query_string();
+        let params: HashMap<String, String> =
+            serde_urlencoded::from_str(query_string).unwrap_or_else(|_| HashMap::new());
 
+        // Get token from Authorization header or from url query string
+        let token_from_param = params
+            .get("token")
+            .map(|value| value.to_owned())
+            .unwrap_or_default();
+
+        let token_str = req
+            .headers()
+            .get("Authorization")
+            .map(|value| value.to_str().unwrap_or_default().to_owned())
+            .unwrap_or(token_from_param);
+
+        if token_str.is_empty() {
             // If the token is not set, allow all requests
             if !db.contains_key(CommunicationToken::token_key()).unwrap() {
                 return futures_util::future::ready(Ok(CommunicationToken("".to_owned())));
             }
-
-            warn!("Token is missing, request from: {}", req.connection_info().realip_remote_addr().unwrap_or("unknown"));
-            futures_util::future::ready(Err(actix_web::error::ErrorUnauthorized("Token is missing")))
+            warn!(
+                "Token is missing, request from: {}",
+                req.connection_info()
+                    .realip_remote_addr()
+                    .unwrap_or("unknown")
+            );
+            futures_util::future::ready(Err(actix_web::error::ErrorUnauthorized(
+                "Token is missing",
+            )))
+        } else {
+            let token = CommunicationToken(token_str.clone());
+            if token.is_valid(db) {
+                futures_util::future::ready(Ok(token))
+            } else {
+                warn!(
+                    "Token: {} is invalid, request from: {}",
+                    token_str,
+                    req.connection_info()
+                        .realip_remote_addr()
+                        .unwrap_or("unknown")
+                );
+                futures_util::future::ready(Err(actix_web::error::ErrorUnauthorized(
+                    "Token is invalid",
+                )))
+            }
         }
     }
 }
