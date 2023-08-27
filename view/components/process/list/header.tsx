b161a87ff5b41fe2a6ef9b8d246a3aec0f3c6c57
ZingerLittleBee <6970999@gmail.com>
2023-08-27 15:56:44 +0800

    feat: 🎸 sort debounce

--- view/components/process/list/header.tsx
+++ view/components/process/list/header.tsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import useWebsocket from "@/hooks/useWebsocket";
 import {Icons} from "@/components/icons";
+import {useDebounceFn} from "ahooks";
 
 export function ProcessListHeader() {
 
@@ -8,68 +9,50 @@ export function ProcessListHeader() {
 
     const [sort, setSort] = useState(new Array(8).fill(false))
 
+    const sortHandler = (sortKey: string, [up, down]: [number, number]) => {
+        if (sort[up]) {
+            setSort(sort.map((_, i) => i === down))
+            sortUp(sortKey)
+        } else if (sort[down]) {
+            setSort(sort.map((_, i) => i === up))
+            sortDown(sortKey)
+        } else {
+            setSort(sort.map((_, i) => i === down))
+            sortUp(sortKey)
+        }
+    }
+
+    const {
+        run: sortRun,
+    } = useDebounceFn<typeof sortHandler>(
+        sortHandler,
+        {
+            wait: 300,
+        }
+    )
+
     return (
         <div className="pl-2 py-2 grid grid-cols-6 text-sm text-muted-foreground bg-muted">
-            <p className="flex col-span-1 truncate items-center space-x-1 cursor-pointer" onClick={() => {
-                if (sort[0]) {
-                    setSort(sort.map((_, i) => i === 1))
-                    sortUp('pid')
-                } else if (sort[1]) {
-                    setSort(sort.map((_, i) => i === 0))
-                    sortDown('pid')
-                } else {
-                    setSort(sort.map((_, i) => i === 1))
-                    sortDown('pid')
-                }
-            }}>
+            <p className="flex col-span-1 truncate items-center space-x-1 cursor-pointer"
+               onClick={() => sortRun('pid', [0, 1])}>
                 <span className="select-none">PID</span>
                 {sort[0] && <Icons.descende size={16}/>}
                 {sort[1] && <Icons.ascende size={16}/>}
             </p>
-            <p className="flex col-span-3 truncate items-center space-x-1 cursor-pointer" onClick={() => {
-                if (sort[2]) {
-                    setSort(sort.map((_, i) => i === 3))
-                    sortUp('name')
-                } else if (sort[3]) {
-                    setSort(sort.map((_, i) => i === 2))
-                    sortDown('name')
-                } else {
-                    setSort(sort.map((_, i) => i === 3))
-                    sortDown('name')
-                }
-            }}>
+            <p className="flex col-span-3 truncate items-center space-x-1 cursor-pointer"
+               onClick={() => sortRun('name', [2, 3])}>
                 <span className="select-none">Name</span>
                 {sort[2] && <Icons.descende size={16}/>}
                 {sort[3] && <Icons.ascende size={16}/>}
             </p>
-            <p className="flex col-span-1 truncate items-center space-x-1 cursor-pointer" onClick={() => {
-                if (sort[4]) {
-                    setSort(sort.map((_, i) => i === 5))
-                    sortUp('cpu')
-                } else if (sort[5]) {
-                    setSort(sort.map((_, i) => i === 4))
-                    sortDown('cpu')
-                } else {
-                    setSort(sort.map((_, i) => i === 5))
-                    sortDown('cpu')
-                }
-            }}>
+            <p className="flex col-span-1 truncate items-center space-x-1 cursor-pointer"
+               onClick={() => sortRun('cpu', [4, 5])}>
                 <span className="select-none">CPU</span>
                 {sort[4] && <Icons.descende size={16}/>}
                 {sort[5] && <Icons.ascende size={16}/>}
             </p>
-            <p className="flex col-span-1 truncate items-center space-x-1 cursor-pointer" onClick={() => {
-                if (sort[6]) {
-                    setSort(sort.map((_, i) => i === 7))
-                    sortUp('mem')
-                } else if (sort[7]) {
-                    setSort(sort.map((_, i) => i === 6))
-                    sortDown('mem')
-                } else {
-                    setSort(sort.map((_, i) => i === 7))
-                    sortDown('mem')
-                }
-            }}>
+            <p className="flex col-span-1 truncate items-center space-x-1 cursor-pointer"
+               onClick={() => sortRun('mem', [6, 7])}>
                 <span className="select-none">Mem</span>
                 {sort[6] && <Icons.descende size={16}/>}
                 {sort[7] && <Icons.ascende size={16}/>}
