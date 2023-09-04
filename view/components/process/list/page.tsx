237c1c28f026d48516f3f161ebb0be74772eb9d9
ZingerLittleBee <6970999@gmail.com>
2023-09-05 01:20:04 +0800

    feat: 🎸 process selected

--- view/components/process/list/page.tsx
+++ view/components/process/list/page.tsx
@@ -6,9 +6,9 @@ import {Virtuoso} from "react-virtuoso";
 import {Card, CardContent} from "@/components/ui/card";
 import {ScrollArea} from "@/components/ui/scroll-area";
 import {STooltip} from "@/components/s-tooltip";
-import {Divider} from "@tremor/react";
 import {ProcessListHeader} from "@/components/process/list/header";
 import useWebsocket from "@/hooks/useWebsocket";
+import {clsx} from "clsx";
 
 export default function ProcessList() {
     const {fusion} = useStore()
@@ -24,20 +24,24 @@ export default function ProcessList() {
         }))
     }, [process])
 
+    const currentProcessId = useMemo(() => {
+        return fusion.current_process?.pid
+    }, [fusion.current_process])
+
     return (
         <Card>
             <ProcessListHeader/>
             <CardContent className="p-0">
-                <ScrollArea className="h-[calc(100vh-65px-1rem-36px-40px-16px-36px-20px)] w-full rounded-md">
+                <ScrollArea className="h-[calc(100vh-65px-1rem-36px-40px-16px-36px-20px)] w-full">
                     <Virtuoso
                         useWindowScroll
                         style={{height: '400px'}}
                         data={processes ?? []}
-                        className="p-0"
+                        className="p-0 rounded-none"
                         itemContent={(_, process) => {
                             return (
-                                <>
-                                    <div className="pl-2 grid grid-cols-6 mt-2 text-[14px]">
+                                <div className={clsx('py-1 border-b', {'bg-slate-300 dark:bg-slate-700' : process.pid === currentProcessId})}>
+                                    <div className="pl-2 grid grid-cols-6 text-[14px]">
                                         <p className="col-span-1 truncate cursor-pointer"
                                            onClick={() => process.pid && requestProcess(process.pid)}>{process.pid}</p>
                                         <STooltip content={process.name}>
@@ -48,9 +52,8 @@ export default function ProcessList() {
                                         <p className="col-span-1 truncate">{process.cpu}</p>
                                         <p className="col-span-1 truncate">{formatToString(process.memory)}</p>
                                     </div>
-                                    <Divider className="my-1"/>
 
-                                </>
+                                </div>
                             )
                         }}
                     />
