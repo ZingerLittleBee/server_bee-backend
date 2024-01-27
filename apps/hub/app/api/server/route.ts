671dd2cf3537e68527b63d69b07001ba7991d86d
ZingerLittleBee <6970999@gmail.com>
2024-01-28 01:01:49 +0800

    feat: 🎸 list server

---
+++ apps/hub/app/api/server/route.ts
@@ -0,0 +1,47 @@
+import { type NextRequest } from 'next/server'
+import { db } from '@/server/db'
+import { getToken } from 'next-auth/jwt'
+
+import { getLogger } from '@/lib/logging'
+
+const logger = getLogger('api:server:route')
+
+export async function GET(req: NextRequest) {
+    try {
+        const token = await getToken({
+            req,
+        })
+
+        logger.info(`token: ${JSON.stringify(token)})`)
+
+        if (!token?.id)
+            return new Response(null, {
+                status: 401,
+            })
+
+        const serverIds = await getServerIdsByUserId(
+            (token as { id: string }).id
+        )
+
+        return new Response(JSON.stringify(serverIds), {
+            status: 200,
+        })
+    } catch {
+        return new Response(null, {
+            status: 401,
+        })
+    }
+}
+
+const getServerIdsByUserId = async (userId: string) => {
+    return (
+        await db.server.findMany({
+            where: {
+                ownerId: userId,
+            },
+            select: {
+                id: true,
+            },
+        })
+    ).map((server) => server.id)
+}
