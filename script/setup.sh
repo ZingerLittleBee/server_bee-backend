12cb226a2c2fcd167098c6640054f2ad1b21280e
勤劳的小蜜蜂 <6970999@gmail.com>
2024-03-23 06:55:52 +0000

    feat: 🎸 setup

--- script/setup.sh
+++ script/setup.sh
@@ -6,6 +6,16 @@ WARNING='\033[1;33m'
 ERROR='\033[1;31m'
 NC='\033[0m' # No Color
 
+# Declare variables
+use_external_mongo=""
+MONGO_INITDB_ROOT_USERNAME=""
+MONGO_INITDB_ROOT_PASSWORD=""
+MONGODB_URI=""
+NEXTAUTH_SECRET=""
+SERVER_JWT_SECRET=""
+SERVER_TOKEN=""
+RECORDER_DOMAIN=""
+
 # Function to check system type
 check_system_type() {
     if [[ "$(uname)" == "Linux" ]]; then
@@ -48,6 +58,7 @@ write_to_env_file() {
 
     cat <<EOF >.env
 RUST_LOG=waring
+DATABASE_URL="file:./serverhub.db"
 NEXTAUTH_SECRET="${nextauth_secret}"
 NEXTAUTH_URL="hub:3000"
 MONGO_INITDB_ROOT_USERNAME="${mongo_initdb_root_username}"
@@ -65,17 +76,19 @@ EOF
 # Function to set MongoDB variables
 set_mongo_variables() {
     read -p "Will you be using an external MongoDB service? (y/n): " use_external_mongo
+    # to lowercase
+    use_external_mongo=${use_external_mongo,,}
 
     if [[ $use_external_mongo == "n" ]]; then
         # Generate random values for MONGO_INITDB_ROOT_USERNAME and MONGO_INITDB_ROOT_PASSWORD
-        MONGO_INITDB_ROOT_USERNAME=$(openssl rand -base64 12)
-        MONGO_INITDB_ROOT_PASSWORD=$(openssl rand -base64 12)
+        MONGO_INITDB_ROOT_USERNAME=$(openssl rand -base64 12 | tr -d '/@:')
+        MONGO_INITDB_ROOT_PASSWORD=$(openssl rand -base64 12 | tr -d '/@:')
 
         # Update MONGODB_URI with the generated values
         MONGODB_URI="mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@mongo:27017/"
     else
         # Prompt user for MONGODB_URI
-        echo -e "${WARNING}Enter the MongoDB URI:${NC}" 
+        echo -e "${WARNING}Enter the MongoDB URI:${NC}"
         read MONGODB_URI
 
         # Remove MONGO_INITDB_ROOT_USERNAME and MONGO_INITDB_ROOT_PASSWORD
@@ -107,60 +120,166 @@ write_to_caddy_file() {
 EOF
 }
 
+installation() {
+    # Prompt user to start installation
+    echo -e "${INFO}Starting application installation...${NC}"
+
+    # Call the function to set MongoDB variables
+    set_mongo_variables
+
+    echo -e "${WARNING}Enter the recorder domain (Example: recorder.serverhub.app):${NC}"
+    read RECORDER_DOMAIN
+    echo -e "${INFO}Make sure to ${RECORDER_DOMAIN} point to the server IP address.${NC}"
+    read -p "Press Enter to continue..."
+
+    # Generate random values
+    NEXTAUTH_SECRET=$(openssl rand -base64 32)
+    SERVER_JWT_SECRET=$(openssl rand -base64 32)
+    SERVER_TOKEN=$(openssl rand -base64 32)
+
+    # Function to print and confirm user input variables
+    print_and_confirm_variables() {
+        echo -e "${INFO}Please reconfirm input variable:${NC}"
+        echo -e "========================================================"
+        if [[ -n $MONGO_INITDB_ROOT_USERNAME ]]; then
+            echo -e "${INFO}MONGO_INITDB_ROOT_USERNAME:${NC} ${WARNING}$MONGO_INITDB_ROOT_USERNAME${NC}"
+        fi
+        if [[ -n $MONGO_INITDB_ROOT_PASSWORD ]]; then
+            echo -e "${INFO}MONGO_INITDB_ROOT_PASSWORD:${NC} ${WARNING}$MONGO_INITDB_ROOT_PASSWORD${NC}"
+        fi
+        echo -e "${INFO}MONGODB_URI:${NC} ${WARNING}$MONGODB_URI${NC}"
+        echo -e "${INFO}NEXTAUTH_SECRET:${NC} ${WARNING}$NEXTAUTH_SECRET${NC}"
+        echo -e "${INFO}SERVER_JWT_SECRET:${NC} ${WARNING}$SERVER_JWT_SECRET${NC}"
+        echo -e "${INFO}SERVER_TOKEN:${NC} ${WARNING}$SERVER_TOKEN${NC}"
+        echo -e "${INFO}RECORDER_DOMAIN:${NC} ${WARNING}$RECORDER_DOMAIN${NC}"
+        echo -e "========================================================"
+
+        # Ask user for confirmation
+        read -p "Are the variables correct? (y/n): " confirm_variables
+
+        if [[ $confirm_variables != "y" ]]; then
+            echo -e "${ERROR}Installation aborted.${NC}"
+            exit 1
+        fi
+    }
+
+    # Call the function to print and confirm variables
+    print_and_confirm_variables
+
+    # Clone GitHub repository
+    git clone https://github.com/ZingerLittleBee/server_bee-backend.git
+    cd server_bee-backend/docker || exit
+
+    # Call the function to set Caddy configuration
+    write_to_caddy_file "$RECORDER_DOMAIN"
+
+    # Write the generated values to .env file using the function
+    write_to_env_file "$NEXTAUTH_SECRET" "$MONGO_INITDB_ROOT_USERNAME" "$MONGO_INITDB_ROOT_PASSWORD" "$MONGODB_URI" "$SERVER_JWT_SECRET" "$SERVER_TOKEN"
+
+    if [[ $use_external_mongo == "y" ]]; then
+        sed -i '/[ \t]*mongo/,/[ \t]*hub/{/[ \t]*hub/!d}' docker-compose.yml
+    fi
+
+    docker-compose up -d
+}
+
+uninstallation() {
+    echo -e "${Waring}Starting application uninstallation...${NC}"
+
+    cd server_bee-backend/docker || exit
+
+    docker-compose down
+
+    echo -e "${ERROR}Whether to remove all data?${NC}"
+    read confirm_remove_data
+
+    if [[ $confirm_remove_data == "y" ]]; then
+        cd ../..
+        rm -rf server_bee-backend
+    fi
+
+    echo -e "${INFO}Uninstallation completed.${NC}"
+}
+
+update() {
+    echo -e "${INFO}Starting application update...${NC}"
+
+    cd server_bee-backend || exit
+
+    git pull
+
+    cd docker || exit
+
+    docker-compose down
+
+    docker-compose up -d
+
+    echo -e "${INFO}Update completed.${NC}"
+}
+
+status() {
+    echo -e "${INFO}Checking application status...${NC}"
+
+    # check docker ps status in the server_bee-backend/docker directory or the current directory
+    if [[ ! -f docker-compose.yml ]]; then
+        cd server_bee-backend/docker || exit
+    fi
+
+    docker-compose ps
+
+    echo "Select service to check logs:"
+    options=("serverhub" "serverbee-recorder" "serverbee-interactor" "serverbee-web" "serverbee-caddy" "serverbee-mongo" "back")
+    select opt in "${options[@]}"; do
+        case $opt in
+        "serverhub")
+            docker logs serverhub
+            ;;
+        "serverbee-recorder")
+            docker logs serverbee-recorder
+            ;;
+        "serverbee-interactor")
+            docker logs serverbee-interactor
+            ;;
+        "serverbee-web")
+            docker logs serverbee-web
+            ;;
+        "serverbee-caddy")
+            docker logs serverbee-caddy
+            ;;
+        "serverbee-mongo")
+            docker logs serverbee-mongo
+            ;;
+        "back")
+            break
+            ;;
+        *) echo "Invalid selection, please try again" ;;
+        esac
+    done
+
+}
+
 check_system_type
 check_tools_installed
 
-# Prompt user to start installation
-echo -e "${INFO}Starting application installation...${NC}"
-
-# Call the function to set MongoDB variables
-set_mongo_variables
-
-echo -e "${WARNING}Enter the recorder domain (Example: recorder.serverhub.app):${NC}"
-read RECORDER_DOMAIN
-
-# Generate random values
-NEXTAUTH_SECRET=$(openssl rand -base64 32)
-SERVER_JWT_SECRET=$(openssl rand -base64 32)
-SERVER_TOKEN=$(openssl rand -base64 32)
-
-# Function to print and confirm user input variables
-print_and_confirm_variables() {
-    echo -e "${INFO}Your input variables:${NC}"
-    echo -e "========================================================"
-    if [[ -n $MONGO_INITDB_ROOT_USERNAME ]]; then
-        echo -e "${INFO}MONGO_INITDB_ROOT_USERNAME:${NC} ${WARNING}$MONGO_INITDB_ROOT_USERNAME${NC}"
-    fi
-    if [[ -n $MONGO_INITDB_ROOT_PASSWORD ]]; then
-        echo -e "${INFO}MONGO_INITDB_ROOT_PASSWORD:${NC} ${WARNING}$MONGO_INITDB_ROOT_PASSWORD${NC}"
-    fi
-    echo -e "${INFO}MONGODB_URI:${NC} ${WARNING}$MONGODB_URI${NC}"
-    echo -e "${INFO}NEXTAUTH_SECRET:${NC} ${WARNING}$NEXTAUTH_SECRET${NC}"
-    echo -e "${INFO}SERVER_JWT_SECRET:${NC} ${WARNING}$SERVER_JWT_SECRET${NC}"
-    echo -e "${INFO}SERVER_TOKEN:${NC} ${WARNING}$SERVER_TOKEN${NC}"
-    echo -e "${INFO}RECORDER_DOMAIN:${NC} ${WARNING}$RECORDER_DOMAIN${NC}"
-    echo -e "========================================================"
-
-    # Ask user for confirmation
-    read -p "Are the variables correct? (y/n): " confirm_variables
-
-    if [[ $confirm_variables != "y" ]]; then
-        echo -e "${ERROR}Installation aborted.${NC}"
-        exit 1
-    fi
-}
-
-# Call the function to print and confirm variables
-print_and_confirm_variables
-
-# Clone GitHub repository
-git clone https://github.com/ZingerLittleBee/server_bee-backend.git
-cd server_bee-backend/docker || exit
-
-# Call the function to set Caddy configuration
-write_to_caddy_file "$RECORDER_DOMAIN"
-
-# Write the generated values to .env file using the function
-write_to_env_file "$NEXTAUTH_SECRET" "$MONGO_INITDB_ROOT_USERNAME" "$MONGO_INITDB_ROOT_PASSWORD" "$MONGODB_URI" "$SERVER_JWT_SECRET" "$SERVER_TOKEN"
-
-docker-compose up -d
+echo "Welcome to use ServerHub automatic installation, please select an option:"
+options=("Installation" "Update" "Uninstallation" "Status" "Exit")
+select opt in "${options[@]}"; do
+    case $opt in
+    "Installation")
+        installation
+        ;;
+    "Update")
+        update
+        ;;
+    "Uninstallation")
+        uninstallation
+        ;;
+    "Status")
+        status
+        ;;
+    "Exit")
+        break
+        ;;
+    *) echo "Invalid selection, please try again" ;;
+    esac
+done
